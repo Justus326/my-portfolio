3492aa340811ea47dcf9f5187b4765f5e736630c
EZE JUSTUS EJIOFOR <justusejiofor326@gmail.com>
2022-12-22 17:51:58 +0100

    Update Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -76,11 +76,11 @@ const Portfolio = () => {
 
                 <div className="flex items-center justify-center">
                    <a href={href1} target="_blank" rel="noreferrer">
-                    <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Demo</button>
+                    <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 bg-gray-400">Demo</button>
                    </a>
 
                    <a href={href2} target="_blank" rel="noreferrer">
-                    <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">Code</button>
+                    <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 bg-gray-400">Code</button>
                   </a>
                     
 
